9455a6eba689b1a847b924f7de20844fb2ea36b0
justindaerolee <daero1995lee@gmail.com>
2016-05-25 21:18:32 +1000

    Update inverted.c

--- inverted.c
+++ inverted.c
@@ -1,27 +1,30 @@
 //inverted.c new implementation
-// find the max CHAR_LEN of the url text file, then use it to malloc a block of mem 
-// with its size, as well as the str to compare with.
-// array of words each with urls
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-#include "tree.h"
+//#include <assert.h>
+#include "searchT.h"
 
 // use tree implementation, left is the different words, right is the urls 
 
-void inverted(char **url, Tree t);
+Tree inverted(char **);
 
-int main(int argc, char **argv) {
-	char **url; // change depending on the implementation
+void printTree(Tree t);
+
+char **getUrl(void); // gets url from collection.txt
+
+int main(void) {
+	char **url;
 	url = getUrl(); // get url and put it into the array
 	Tree t = newTree();
-	t = inverted(url, t);
-	showTree(t); // alittle different to the orginal showTree
+	t = inverted(url);
+	printTree(t); // alittle different to the orginal showTree
 	freeTree(t);
+	return 0;
 }
-Tree inverted(char **url, Tree t) {
+Tree inverted(char **url) {
 	Tree t = newTree(); // tree.h
 	int i, j, k;
 	FILE *fp;
@@ -46,7 +49,7 @@ Tree inverted(char **url, Tree t) {
 		fclose(fp);
 
 		fp = fopen(url[i], "r");
-		for (fgets(str, maxStrLen, fp); strchr(str, "#start Section-2") != NULL &&
+		for (fgets(str, maxStrLen, fp); strncmp(str, "#start Section-2", 16) != 0 &&
 			str != NULL; fgets(str, maxStrLen, fp));
 
 		while (fgets(str,maxStrLen, fp) != NULL) {
@@ -56,8 +59,8 @@ Tree inverted(char **url, Tree t) {
 				}
 				word[k] = '\0';
 				if (!wordIsInT(t, word)) { // make function
-					insertWordT(t, word); //make function
-					insetUrlT(t, url[i], word); // make function
+					t = insertWordT(t, word); //make function
+					t = insetUrlT(t, url[i], word); // make function
 				}
 				
 			}
@@ -66,5 +69,58 @@ Tree inverted(char **url, Tree t) {
 		free(word);
 		fclose(fp);
 	}
+	return t;
+}
+void printTree(Tree t) {
+	Tree temp1 = t;
+	Tree temp2 = t;
+	FILE *fp = fopen("invertedIndex.txt", "w");
+	while (temp1 != NULL) {
+		fprintf(fp, "%s", temp1->str);
+		temp2 = temp1->right;
+		while (temp2 != NULL) {
+			fprintf(fp, " %s", temp2->str);
+			temp2 = temp2->right;
+		}
+		putchar('\n');
+		temp1 = temp1->left;
+	}
+	fclose(fp);
+}
+char **getUrl(void) { // gets url from collection.txt
+	FILE *fp = fopen("collection.txt", "r");
+	char c;
+	char **url;
+	int urlCount = 0;
+	int i, j;
 
+
+	
+	for (c = fgetc(fp); c != EOF; c = fgetc(fp)) {
+		if (!isspace(c)) {
+			urlCount ++;
+		}
+		while (!isspace(c) || c != EOF) {
+			c = fgetc(fp);
+		}
+	}
+	url = malloc(4 * urlCount); // change to be specific
+	for (i = 0; i < urlCount; i ++) {
+		url[i] = malloc(sizeof(char) * 50);
+	}
+	fclose(fp);
+	fp = fopen("collection.txt", "r");	
+	for (c = fgetc(fp), j = 0; c != EOF; c = fgetc(fp), j ++) {
+		if (!isspace(c)) {
+			for (i = 0; !isspace(c) && c != '\0' && c != EOF; i ++, c = fgetc(fp)) {
+				url[j][i] = c;
+			}
+			url[j][i] = '\0';
+		}
+	}
+	for (i = 0; i < urlCount; i ++) {
+		printf("%s ", url[i]);
+	}
+	putchar('\n');
+	return url;
 }
